bf518001c0dd876dfe4dea4d8721cea446171f12
Emmanuel <emmayodayo@gmail.com>
2025-05-04 05:16:27 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,19 @@ import { AuroraBackground } from "@/components/ui/aurora-background";
 import { HeroSectionOne } from "@/components/ui/hero-section-one";
 import { FeaturesSectionDemo } from "@/components/ui/features-section-demo";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+// import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 
 export default function Home() {
 	return (
 		<div>
-			<ModeToggle/>
+			<ModeToggle />
 			<AuroraBackground>
+			{/* <BackgroundGradientAnimation> */}
 				<HeroSectionOne />
+			{/* </BackgroundGradientAnimation> */}
 			</AuroraBackground>
 
-			<FeaturesSectionDemo/>
+			<FeaturesSectionDemo />
 		</div>
 	);
 }
--- src/components/ui/features-section-demo.tsx
+++ src/components/ui/features-section-demo.tsx
@@ -2,27 +2,27 @@ import React from "react";
 import { cn } from "@/lib/utils";
 import createGlobe from "cobe";
 import { useEffect, useRef } from "react";
-import { motion } from "motion/react";
+// import { motion } from "motion/react";
 import { IconBrandYoutubeFilled } from "@tabler/icons-react";
 
 
 export function FeaturesSectionDemo() {
   const features = [
-    {
-      title: "Track issues effectively",
-      description:
-        "Track and manage your project issues with ease using our intuitive interface.",
-      skeleton: <SkeletonOne />,
-      className:
-        "col-span-1 lg:col-span-4 border-b lg:border-r dark:border-neutral-800",
-    },
-    {
-      title: "Capture pictures with AI",
-      description:
-        "Capture stunning photos effortlessly using our advanced AI technology.",
-      skeleton: <SkeletonTwo />,
-      className: "border-b col-span-1 lg:col-span-2 dark:border-neutral-800",
-    },
+    // {
+    //   title: "Track issues effectively",
+    //   description:
+    //     "Track and manage your project issues with ease using our intuitive interface.",
+    //   skeleton: <SkeletonOne />,
+    //   className:
+    //     "col-span-1 lg:col-span-4 border-b lg:border-r dark:border-neutral-800",
+    // },
+    // {
+    //   title: "Capture pictures with AI",
+    //   description:
+    //     "Capture stunning photos effortlessly using our advanced AI technology.",
+    //   skeleton: <SkeletonTwo />,
+    //   className: "border-b col-span-1 lg:col-span-2 dark:border-neutral-800",
+    // },
     {
       title: "Watch our AI on YouTube",
       description:
@@ -38,6 +38,33 @@ export function FeaturesSectionDemo() {
       skeleton: <SkeletonFour />,
       className: "col-span-1 lg:col-span-3 border-b lg:border-none",
     },
+	{
+		title: "Deploy in seconds",
+		description:
+		  "With our blazing fast, state of the art, cutting edge, we are so back cloud servies (read AWS) - you can deploy your model in seconds.",
+		skeleton: <SkeletonFour />,
+		className: "col-span-1 lg:col-span-3 border-b lg:border-none",
+	  },
+	  {
+		title: "Deploy in seconds",
+		description:
+		  "With our blazing fast, state of the art, cutting edge, we are so back cloud servies (read AWS) - you can deploy your model in seconds.",
+		skeleton: <SkeletonFour />,
+		className: "col-span-1 lg:col-span-3 border-b lg:border-none",
+	  },
+	  {
+		title: "Deploy in seconds",
+		description:
+		  "With our blazing fast, state of the art, cutting edge, we are so back cloud servies (read AWS) - you can deploy your model in seconds.",
+		skeleton: <SkeletonFour />,
+		className: "col-span-1 lg:col-span-3 border-b lg:border-none",
+	  },{
+		title: "Deploy in seconds",
+		description:
+		  "With our blazing fast, state of the art, cutting edge, we are so back cloud servies (read AWS) - you can deploy your model in seconds.",
+		skeleton: <SkeletonFour />,
+		className: "col-span-1 lg:col-span-3 border-b lg:border-none",
+	  },
   ];
   return (
     <div className="relative z-20 py-10 lg:py-40 max-w-7xl mx-auto">
@@ -149,133 +176,133 @@ export const SkeletonThree = () => {
   );
 };
 
-export const SkeletonTwo = () => {
-  const images = [
-    "https://images.unsplash.com/photo-1517322048670-4fba75cbbb62?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1573790387438-4da905039392?q=80&w=3425&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1555400038-63f5ba517a47?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1554931670-4ebfabf6e7a9?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1546484475-7f7bd55792da?q=80&w=2581&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  ];
+// export const SkeletonTwo = () => {
+//   const images = [
+//     "https://images.unsplash.com/photo-1517322048670-4fba75cbbb62?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     "https://images.unsplash.com/photo-1573790387438-4da905039392?q=80&w=3425&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     "https://images.unsplash.com/photo-1555400038-63f5ba517a47?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     "https://images.unsplash.com/photo-1554931670-4ebfabf6e7a9?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     "https://images.unsplash.com/photo-1546484475-7f7bd55792da?q=80&w=2581&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//   ];
 
-  const imageVariants = {
-    whileHover: {
-      scale: 1.1,
-      rotate: 0,
-      zIndex: 100,
-    },
-    whileTap: {
-      scale: 1.1,
-      rotate: 0,
-      zIndex: 100,
-    },
-  };
-  return (
-    <div className="relative flex flex-col items-start p-8 gap-10 h-full overflow-hidden">
-      {/* TODO */}
-      <div className="flex flex-row -ml-20">
-        {images.map((image, idx) => (
-          <motion.div
-            variants={imageVariants}
-            key={"images-first" + idx}
-            style={{
-              rotate: Math.random() * 20 - 10,
-            }}
-            whileHover="whileHover"
-            whileTap="whileTap"
-            className="rounded-xl -mr-4 mt-4 p-1 bg-white dark:bg-neutral-800 dark:border-neutral-700 border border-neutral-100 shrink-0 overflow-hidden"
-          >
-            <img
-              src={image}
-              alt="bali images"
-              width="500"
-              height="500"
-              className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover shrink-0"
-            />
-          </motion.div>
-        ))}
-      </div>
-      <div className="flex flex-row">
-        {images.map((image, idx) => (
-          <motion.div
-            key={"images-second" + idx}
-            style={{
-              rotate: Math.random() * 20 - 10,
-            }}
-            variants={imageVariants}
-            whileHover="whileHover"
-            whileTap="whileTap"
-            className="rounded-xl -mr-4 mt-4 p-1 bg-white dark:bg-neutral-800 dark:border-neutral-700 border border-neutral-100 shrink-0 overflow-hidden"
-          >
-            <img
-              src={image}
-              alt="bali images"
-              width="500"
-              height="500"
-              className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover shrink-0"
-            />
-          </motion.div>
-        ))}
-      </div>
+//   const imageVariants = {
+//     whileHover: {
+//       scale: 1.1,
+//       rotate: 0,
+//       zIndex: 100,
+//     },
+//     whileTap: {
+//       scale: 1.1,
+//       rotate: 0,
+//       zIndex: 100,
+//     },
+//   };
+//   return (
+//     <div className="relative flex flex-col items-start p-8 gap-10 h-full overflow-hidden">
+//       {/* TODO */}
+//       <div className="flex flex-row -ml-20">
+//         {images.map((image, idx) => (
+//           <motion.div
+//             variants={imageVariants}
+//             key={"images-first" + idx}
+//             style={{
+//               rotate: Math.random() * 20 - 10,
+//             }}
+//             whileHover="whileHover"
+//             whileTap="whileTap"
+//             className="rounded-xl -mr-4 mt-4 p-1 bg-white dark:bg-neutral-800 dark:border-neutral-700 border border-neutral-100 shrink-0 overflow-hidden"
+//           >
+//             <img
+//               src={image}
+//               alt="bali images"
+//               width="500"
+//               height="500"
+//               className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover shrink-0"
+//             />
+//           </motion.div>
+//         ))}
+//       </div>
+//       <div className="flex flex-row">
+//         {images.map((image, idx) => (
+//           <motion.div
+//             key={"images-second" + idx}
+//             style={{
+//               rotate: Math.random() * 20 - 10,
+//             }}
+//             variants={imageVariants}
+//             whileHover="whileHover"
+//             whileTap="whileTap"
+//             className="rounded-xl -mr-4 mt-4 p-1 bg-white dark:bg-neutral-800 dark:border-neutral-700 border border-neutral-100 shrink-0 overflow-hidden"
+//           >
+//             <img
+//               src={image}
+//               alt="bali images"
+//               width="500"
+//               height="500"
+//               className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover shrink-0"
+//             />
+//           </motion.div>
+//         ))}
+//       </div>
 
-      <div className="absolute left-0 z-[100] inset-y-0 w-20 bg-gradient-to-r from-white dark:from-black to-transparent  h-full pointer-events-none" />
-      <div className="absolute right-0 z-[100] inset-y-0 w-20 bg-gradient-to-l from-white dark:from-black  to-transparent h-full pointer-events-none" />
-    </div>
-  );
-};
+//       <div className="absolute left-0 z-[100] inset-y-0 w-20 bg-gradient-to-r from-white dark:from-black to-transparent  h-full pointer-events-none" />
+//       <div className="absolute right-0 z-[100] inset-y-0 w-20 bg-gradient-to-l from-white dark:from-black  to-transparent h-full pointer-events-none" />
+//     </div>
+//   );
+// };
 
 export const SkeletonFour = () => {
   return (
     <div className="h-60 md:h-60  flex flex-col items-center relative bg-transparent dark:bg-transparent mt-10">
-      <Globe className="absolute -right-10 md:-right-10 -bottom-80 md:-bottom-72" />
+      <img className="absolute -right-10 md:-right-10 -bottom-80 md:-bottom-72" src='https://images.unsplash.com/photo-1517322048670-4fba75cbbb62?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
     </div>
   );
 };
 
-export const Globe = ({ className }: { className?: string }) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+// export const Globe = ({ className }: { className?: string }) => {
+//   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
-    let phi = 0;
+//   useEffect(() => {
+//     let phi = 0;
 
-    if (!canvasRef.current) return;
+//     if (!canvasRef.current) return;
 
-    const globe = createGlobe(canvasRef.current, {
-      devicePixelRatio: 2,
-      width: 600 * 2,
-      height: 600 * 2,
-      phi: 0,
-      theta: 0,
-      dark: 1,
-      diffuse: 1.2,
-      mapSamples: 16000,
-      mapBrightness: 6,
-      baseColor: [0.3, 0.3, 0.3],
-      markerColor: [0.1, 0.8, 1],
-      glowColor: [1, 1, 1],
-      markers: [
-        // longitude latitude
-        { location: [37.7595, -122.4367], size: 0.03 },
-        { location: [40.7128, -74.006], size: 0.1 },
-      ],
-      onRender: (state) => {
-        // Called on every animation frame.
-        // `state` will be an empty object, return updated params.
-        state.phi = phi;
-        phi += 0.01;
-      },
-    });
+//     const globe = createGlobe(canvasRef.current, {
+//       devicePixelRatio: 2,
+//       width: 600 * 2,
+//       height: 600 * 2,
+//       phi: 0,
+//       theta: 0,
+//       dark: 1,
+//       diffuse: 1.2,
+//       mapSamples: 16000,
+//       mapBrightness: 6,
+//       baseColor: [0.3, 0.3, 0.3],
+//       markerColor: [0.1, 0.8, 1],
+//       glowColor: [1, 1, 1],
+//       markers: [
+//         // longitude latitude
+//         { location: [37.7595, -122.4367], size: 0.03 },
+//         { location: [40.7128, -74.006], size: 0.1 },
+//       ],
+//       onRender: (state) => {
+//         // Called on every animation frame.
+//         // `state` will be an empty object, return updated params.
+//         state.phi = phi;
+//         phi += 0.01;
+//       },
+//     });
 
-    return () => {
-      globe.destroy();
-    };
-  }, []);
+//     return () => {
+//       globe.destroy();
+//     };
+//   }, []);
 
-  return (
-    <canvas
-      ref={canvasRef}
-      style={{ width: 600, height: 600, maxWidth: "100%", aspectRatio: 1 }}
-      className={className}
-    />
-  );
-};
+//   return (
+//     <canvas
+//       ref={canvasRef}
+//       style={{ width: 600, height: 600, maxWidth: "100%", aspectRatio: 1 }}
+//       className={className}
+//     />
+//   );
+// };
