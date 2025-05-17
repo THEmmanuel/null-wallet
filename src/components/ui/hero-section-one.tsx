7503d6958064537e7765c9b2079c62574575310d
Emmanuel <emmayodayo@gmail.com>
2025-05-17 03:17:04 +0100

    update

--- src/components/ui/hero-section-one.tsx
+++ src/components/ui/hero-section-one.tsx
@@ -2,6 +2,7 @@
 
 
 import { motion } from "motion/react";
+import Link from "next/link"
 
 export function HeroSectionOne() {
 	return (
@@ -66,9 +67,11 @@ export function HeroSectionOne() {
 					}}
 					className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
 				>
-					<button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-						Launch App
-					</button>
+					<Link href="/home">
+						<button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+							Launch App
+						</button>
+					</Link>
 					<button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
 						Get Started
 					</button>
@@ -100,7 +103,7 @@ export function HeroSectionOne() {
 				</motion.div>
 
 			</div>
-		</div>
+		</div >
 	);
 }
 
